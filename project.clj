20aa737a82733e7e3d1b33684601453d6224d222
avasdowney <avasdowney@gmail.com>
2021-06-26 15:44:45 -0400

    added joke, and user retreival

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject reddit-scraper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Reddit Scraper"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [enlive "1.1.1"]
+                 [ring "1.2.0"]
+                 [clj-http "3.12.0"]
+                 [cheshire "5.9.0"]
+                 [net.cgrand/moustache "1.1.0"]]
   :main ^:skip-aot reddit-scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/reddit_scraper/core.clj
+++ src/reddit_scraper/core.clj
@@ -1,7 +1,37 @@
 (ns reddit-scraper.core
-  (:gen-class))
+  (:require [net.cgrand.enlive-html :as html]
+            [clj-http.client :as client]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+"We need a User-Agent to pretend we're a browser"
+(def h {"User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"})
+
+"sets url to pull from"
+(def ^:dynamic *dad-jokes-url* "https://www.reddit.com/r/dadjokes/hot.json")
+
+"pulls from java.net.url class to retrieve url... not needed as fetch-json used instead"
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+"pull structured json file directly from reddit"
+(defn fetch-json [url]
+  (:body (client/get url {:headers h :as :json-strict})))
+
+"fetches all of the dad jokes, returns a list of the raw jokes"
+(defn dad-jokes-raw []
+  (:children (:data (fetch-json *dad-jokes-url*)))
+  )
+
+"pulls all data from child array into a list"
+(defn dad-jokes-data []
+  (map #(:data %) (dad-jokes-raw))
+  )
+
+"given the raw data, pull out just what we want"
+(defn select-joke [data]
+  (select-keys data [:title :selftext :author])
+  )
+
+"show all of the jokes with just what want"
+(defn dad-jokes []
+  (map #(select-joke %) (dad-jokes-data))
+  )
